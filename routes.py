9923af4268350c1d23bc1cb0cec682f0be04bce1
HEnquist <henrik.enquist@gmail.com>
2020-08-10 09:12:38 +0200

    Update routes

--- routes.py
+++ routes.py
@@ -5,6 +5,44 @@ from camilladsp.plot_filters import plot_filter
 
 view = Blueprint("view", __name__)
 
+@view.route('/getparam/<name>', methods=['GET', 'POST'])
+def get_param(name):
+    name = name.lower()
+    cdsp = current_app.config['CAMILLA']
+    if name == "state":
+        result = cdsp.get_state()
+    elif name == "signalrange":
+        result = cdsp.get_signal_range()
+    elif name == "signalrangedB":
+        result = cdsp.get_signal_range_dB()
+    elif name == "capturerateraw":
+        result = cdsp.get_capture_rate_raw()
+    elif name == "capturerate":
+        result = cdsp.get_capture_rate()
+    elif name == "rateadjust":
+        result = cdsp.get_capture_rate_raw()
+    elif name == "updateinterval":
+        result = cdsp.get_update_interval()
+    elif name == "configname":
+        result = cdsp.get_config_name()     
+    elif name == "configraw":
+        result = cdsp.get_config_raw()
+    else:
+        result = "ERROR"
+    return str(result)
+
+@view.route('/setparam/<name>', methods=['GET', 'POST'])
+def set_param(name):
+    value = request.get_data()
+    name = name.lower()
+    cdsp = current_app.config['CAMILLA']
+    if name == "updateinterval":
+        cdsp.set_update_interval(value)
+    elif name == "configname":
+        cdsp.set_config_name(value)     
+    elif name == "configraw":
+        cdsp.set_config_raw(value)
+    return "OK"
 
 @view.route('/filter/<name>', methods=['GET', 'POST'])
 def eval_filter(name):
@@ -35,9 +73,3 @@ def get_version():
     version = {"major": vers_tup[0], "minor": vers_tup[1], "patch": vers_tup[2]}
     return version
 
-@view.route('/state', methods=['GET', 'POST'])
-def get_state():
-    # print(content) # Do your processing
-    cdsp = current_app.config['CAMILLA']
-    state = cdsp.get_state()
-    return state
