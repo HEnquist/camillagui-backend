8620ff44062801f430d5f0218693a3e2ee8fb96d
HEnquist <henrik.enquist@gmail.com>
2020-08-17 23:30:33 +0200

    add routes

--- routes.py
+++ routes.py
@@ -2,10 +2,11 @@ from flask import Blueprint, render_template
 from flask import Flask, request, send_file, current_app
 from camilladsp import plot_pipeline
 from camilladsp import plot_filter
+import yaml
 
 view = Blueprint("view", __name__)
 
-@view.route('/getparam/<name>', methods=['GET', 'POST'])
+@view.route('/api/getparam/<name>', methods=['GET', 'POST'])
 def get_param(name):
     name = name.lower()
     print(name)
@@ -33,7 +34,7 @@ def get_param(name):
     print(result)
     return str(result)
 
-@view.route('/setparam/<name>', methods=['GET', 'POST'])
+@view.route('/api/setparam/<name>', methods=['GET', 'POST'])
 def set_param(name):
     value = request.get_data().decode()
     name = name.lower()
@@ -47,30 +48,53 @@ def set_param(name):
         cdsp.set_config_raw(value)
     return "OK"
 
-@view.route('/filter', methods=['GET', 'POST'])
+@view.route('/api/evalfilter', methods=['GET', 'POST'])
 def eval_filter():
     content = request.get_json(silent=True)
     print("content", content)
     image=plot_filter(content["config"], name=content["name"], samplerate=content["samplerate"], npoints=1000, toimage=True)
     return send_file(image, mimetype='image/svg+xml')
 
-@view.route('/pipeline', methods=['GET', 'POST'])
+@view.route('/api/evalpipeline', methods=['GET', 'POST'])
 def eval_pipeline():
     content = request.get_json(silent=True)
     print("content", content)
     image=plot_pipeline(content, toimage=True)
     return send_file(image, mimetype='image/svg+xml')
 
-@view.route('/getconfig', methods=['GET', 'POST'])
+@view.route('/api/getconfig', methods=['GET', 'POST'])
 def get_config():
     cdsp = current_app.config['CAMILLA']
     config = cdsp.get_config()
     return config
 
-@view.route('/version', methods=['GET', 'POST'])
+@view.route('/api/setconfig', methods=['GET', 'POST'])
+def set_config():
+    content = request.get_json(silent=True)
+    print("content", content)
+    cdsp = current_app.config['CAMILLA']
+    cdsp.set_config(content)
+    return "OK"
+
+@view.route('/api/configtoyml', methods=['GET', 'POST'])
+def config_to_yml():
+    content = request.get_json(silent=True)
+    print("content", content)
+    conf_yml = yaml.dump(content)
+    return conf_yml
+
+@view.route('/api/ymltojson', methods=['GET', 'POST'])
+def yml_to_json():
+    data_yml = request.get_data().decode()
+    data = yaml.safe_load(data_yml)
+    return data
+
+@view.route('/api/version', methods=['GET', 'POST'])
 def get_version():
     cdsp = current_app.config['CAMILLA']
     vers_tup = cdsp.get_version()
     version = {"major": vers_tup[0], "minor": vers_tup[1], "patch": vers_tup[2]}
     return version
+
+
 
