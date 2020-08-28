19477e1cd1ce751792c3a881209ab45a977baa5c
HEnquist <henrik.enquist@gmail.com>
2020-08-28 23:50:48 +0200

    Many fixes

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from routes import setup_routes, setup_static_routes
 app = web.Application()
 setup_routes(app)
 setup_static_routes(app)
-camillaconnection = CamillaConnection(config["host"], config["port"])
+camillaconnection = CamillaConnection(config["camilla_host"], config["camilla_port"])
 camillaconnection.connect()
 app['CAMILLA'] = camillaconnection
-web.run_app(app)
+web.run_app(app, port=config["port"])
