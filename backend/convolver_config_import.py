5fd0fa7ced6f45d83042baceac78f5e93f017d57
Henrik <henrik.enquist@gmail.com>
2024-01-20 21:43:22 +0100

    Fix parsing of gain, format code

--- backend/convolver_config_import.py
+++ backend/convolver_config_import.py
@@ -1,19 +1,41 @@
-from os.path import split
+from os.path import basename
 
 
 def filename_of_path(path: str) -> str:
-    return split(path.replace('\\', '/'))[-1]
+    """
+    Return just the filename from a full path.
+    Accepts both Windows paths such as C:\temp\file.wav
+    and Unix paths such as /tmp/file.wav
+    """
+    return basename(path.replace("\\", "/"))
 
 
-def channels_factors_and_inversions_as_list(channels_and_factors: str) -> list[tuple[int, float, bool]]:
-    channels_and_fractions = [channel_and_fraction.split('.')
-                              for channel_and_fraction
-                              in channels_and_factors.split(' ')]
+def fraction_to_gain(fraction: str) -> float:
+    if int(fraction) == 0:
+        # Special case, n.0 means channel n with a linear gain of 1.0
+        return 1.0
+    # n.mmm means channel n with a linear gain of 0.mmm
+    return float(f"0.{fraction}")
+
+
+def parse_channel_and_fraction(channel: str, fraction: str) -> (int, float, bool):
+    int_channel = abs(int(channel))
+    gain = fraction_to_gain(fraction)
+    inverted = channel.startswith("-")
+    return (abs(int_channel), gain, inverted)
+
+
+def channels_factors_and_inversions_as_list(
+    channels_and_factors: str,
+) -> list[tuple[int, float, bool]]:
+    channels_and_fractions = [
+        channel_and_fraction.split(".")
+        for channel_and_fraction in channels_and_factors.split(" ")
+    ]
     return [
-        (abs(int(channel)),
-         float('0.' + fraction),
-         channel[0] == '-')
-        for (channel, fraction) in channels_and_fractions]
+        parse_channel_and_fraction(channel, fraction)
+        for (channel, fraction) in channels_and_fractions
+    ]
 
 
 class Filter:
@@ -31,11 +53,10 @@ class Filter:
         self.output_channels = channels_factors_and_inversions_as_list(filter_text[3])
 
     def name(self) -> str:
-        return self.filename + '-' + str(self.channel_in_file)
+        return self.filename + "-" + str(self.channel_in_file)
 
 
 class ConvolverConfig:
-
     _samplerate: int
     _input_channels: int
     _output_channels: int
@@ -54,21 +75,23 @@ class ConvolverConfig:
         self._output_channels = int(first_line_items[2])
         self._input_delays = [int(x) for x in lines[1].split()]
         self._output_delays = [int(x) for x in lines[2].split()]
-        filter_lines = lines[3:len(lines)]
+        filter_lines = lines[3 : len(lines)]
         filter_count = int(len(filter_lines) / 4)
-        self._filters = [Filter(n, filter_lines[n*4:n*4+4]) for n in range(filter_count)]
+        self._filters = [
+            Filter(n, filter_lines[n * 4 : n * 4 + 4]) for n in range(filter_count)
+        ]
 
     def to_object(self) -> dict:
         return {
-            'devices': {'samplerate': self._samplerate},
-            'filters': self._delay_filter_definitions() | self._convolution_filter_definitions(),
-            'mixers': self._mixer_in() | self._mixer_out(),
-            'pipeline':
-                self._input_delay_pipeline_steps()
-                + self._mixer_in_pipeline_step()
-                + self._filter_pipeline_steps()
-                + self._mixer_out_pipeline_step()
-                + self._output_delay_pipeline_steps()
+            "devices": {"samplerate": self._samplerate},
+            "filters": self._delay_filter_definitions()
+            | self._convolution_filter_definitions(),
+            "mixers": self._mixer_in() | self._mixer_out(),
+            "pipeline": self._input_delay_pipeline_steps()
+            + self._mixer_in_pipeline_step()
+            + self._filter_pipeline_steps()
+            + self._mixer_out_pipeline_step()
+            + self._output_delay_pipeline_steps(),
         }
 
     def _delay_filter_definitions(self) -> dict:
@@ -78,20 +101,24 @@ class ConvolverConfig:
 
     @staticmethod
     def _delay_name(delay: int) -> str:
-        return 'Delay'+str(delay)
+        return "Delay" + str(delay)
 
     @staticmethod
     def _delay_filter(delay: int) -> dict:
         return {
-            'type': 'Delay',
-            'parameters': {'delay': delay, 'unit': 'ms', 'subsample': False}
+            "type": "Delay",
+            "parameters": {"delay": delay, "unit": "ms", "subsample": False},
         }
 
     def _convolution_filter_definitions(self) -> dict:
         return {
             f.name(): {
-                'type': 'Conv',
-                'parameters': {'type': 'Wav', 'filename': f.filename, 'channel': f.channel_in_file}
+                "type": "Conv",
+                "parameters": {
+                    "type": "Wav",
+                    "filename": f.filename,
+                    "channel": f.channel_in_file,
+                },
             }
             for f in self._filters
         }
@@ -102,13 +129,14 @@ class ConvolverConfig:
     def _delay_pipeline_steps(self, delays: list[int]) -> list[dict]:
         return [
             {
-                'type': 'Filter',
-                'channel': channel,
-                'names': [self._delay_name(delay)],
-                'bypassed': None,
-                'description': None,
+                "type": "Filter",
+                "channel": channel,
+                "names": [self._delay_name(delay)],
+                "bypassed": None,
+                "description": None,
             }
-            for channel, delay in enumerate(delays) if delay != 0
+            for channel, delay in enumerate(delays)
+            if delay != 0
         ]
 
     def _output_delay_pipeline_steps(self) -> list[dict]:
@@ -116,52 +144,72 @@ class ConvolverConfig:
 
     def _mixer_in(self) -> dict:
         return {
-            'Mixer in': {
-                'channels': {
-                    'in': self._input_channels,
-                    'out': max(1, len(self._filters))
+            "Mixer in": {
+                "channels": {
+                    "in": self._input_channels,
+                    "out": max(1, len(self._filters)),
                 },
-                'mapping': [
-                    {'dest': f.channel,
-                     'sources': [
-                         {'channel': channel, 'gain': factor, 'scale': 'linear', 'inverted': invert}
-                         for (channel, factor, invert) in f.input_channels
-                     ]}
+                "mapping": [
+                    {
+                        "dest": f.channel,
+                        "sources": [
+                            {
+                                "channel": channel,
+                                "gain": factor,
+                                "scale": "linear",
+                                "inverted": invert,
+                            }
+                            for (channel, factor, invert) in f.input_channels
+                        ],
+                    }
                     for f in self._filters
-                ]
+                ],
             }
         }
 
     def _mixer_out(self) -> dict:
         return {
-            'Mixer out': {
-                'channels': {
-                    'in': max(1, len(self._filters)),
-                    'out': self._output_channels
+            "Mixer out": {
+                "channels": {
+                    "in": max(1, len(self._filters)),
+                    "out": self._output_channels,
                 },
-                'mapping': [
-                    {'dest': output_channel,
-                     'sources': [
-                         {'channel': f.channel, 'gain': factor, 'scale': 'linear', 'inverted': invert}
-                         for f in self._filters
-                         for (channel, factor, invert) in f.output_channels
-                         if channel == output_channel
-                     ]}
+                "mapping": [
+                    {
+                        "dest": output_channel,
+                        "sources": [
+                            {
+                                "channel": f.channel,
+                                "gain": factor,
+                                "scale": "linear",
+                                "inverted": invert,
+                            }
+                            for f in self._filters
+                            for (channel, factor, invert) in f.output_channels
+                            if channel == output_channel
+                        ],
+                    }
                     for output_channel in range(self._output_channels)
-                ]
+                ],
             }
         }
 
     @staticmethod
     def _mixer_in_pipeline_step() -> list[dict]:
-        return [{'type': 'Mixer', 'name': 'Mixer in', 'description': None}]
+        return [{"type": "Mixer", "name": "Mixer in", "description": None}]
 
     @staticmethod
     def _mixer_out_pipeline_step() -> list[dict]:
-        return [{'type': 'Mixer', 'name': 'Mixer out', 'description': None}]
+        return [{"type": "Mixer", "name": "Mixer out", "description": None}]
 
     def _filter_pipeline_steps(self) -> list[dict]:
         return [
-            {'type': 'Filter', 'channel': f.channel, 'names': [f.name()], 'bypassed': None, 'description': None}
+            {
+                "type": "Filter",
+                "channel": f.channel,
+                "names": [f.name()],
+                "bypassed": None,
+                "description": None,
+            }
             for f in self._filters
         ]
