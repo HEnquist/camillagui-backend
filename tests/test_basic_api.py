72a5b9bb3dbcd3654b44f52ba592afc01830027d
Henrik <henrik.enquist@gmail.com>
2024-01-24 21:53:17 +0100

    Add tests for config handling

--- tests/test_basic_api.py
+++ tests/test_basic_api.py
@@ -14,6 +14,10 @@ import camilladsp
 
 TESTFILE_DIR = os.path.join(os.path.dirname(__file__), "testfiles")
 SAMPLE_CONFIG = yaml.safe_load(open(os.path.join(TESTFILE_DIR, "config.yml")))
+statefile_data = yaml.safe_load(open(os.path.join(TESTFILE_DIR, "statefile_template.yml")))
+statefile_data["config_path"] = os.path.join(TESTFILE_DIR, "config2.yml")
+with open(os.path.join(TESTFILE_DIR, "statefile.yml"), "w") as f:
+    yaml.dump(statefile_data, f)
 
 server_config = {
     "camilla_host": "127.0.0.1",
@@ -39,51 +43,62 @@ def mock_request(mock_app):
     request.app = mock_app
     yield request
 
-
 @pytest.fixture
-def mock_app():
+def mock_camillaclient():
     client = MagicMock()
     client_constructor = MagicMock(return_value=client)
+    client_constructor._client = client
     client.volume = MagicMock()
-    client.volume.main = MagicMock(side_effect=[-20.0])
+    client.volume.main = MagicMock(return_value=-20.0)
     client.mute = MagicMock()
-    client.mute.main = MagicMock(side_effect=[False])
+    client.mute.main = MagicMock(return_value=False)
     client.levels = MagicMock
-    client.levels.capture_peak = MagicMock(side_effect=[[-2.0, -3.0]])
-    client.levels.playback_peak = MagicMock(side_effect=[[-2.5, -3.5]])
+    client.levels.capture_peak = MagicMock(return_value=[-2.0, -3.0])
+    client.levels.playback_peak = MagicMock(return_value=[-2.5, -3.5])
     client.levels.levels = MagicMock(
-        side_effect=[
-            {
-                "capture_rms": [-5.0, -6.0],
-                "capture_peak": [-2.0, -3.0],
-                "playback_rms": [-7.0, -8.0],
-                "playback_peak": [-3.0, -4.0],
-            }
-        ]
+        return_value={
+            "capture_rms": [-5.0, -6.0],
+            "capture_peak": [-2.0, -3.0],
+            "playback_rms": [-7.0, -8.0],
+            "playback_peak": [-3.0, -4.0],
+        }
     )
     client.rate = MagicMock()
-    client.rate.capture = MagicMock(side_effect=[44100])
+    client.rate.capture = MagicMock(return_value=44100)
     client.general = MagicMock()
     client.general.state = MagicMock(
-        side_effect=[camilladsp.camilladsp.ProcessingState.RUNNING]
+        return_value=camilladsp.camilladsp.ProcessingState.RUNNING
     )
     client.general.list_capture_devices = MagicMock(
-        side_effect=[[["hw:Aaaa,0,0", "Dev A"], ["hw:Bbbb,0,0", "Dev B"]]]
+        return_value=[["hw:Aaaa,0,0", "Dev A"], ["hw:Bbbb,0,0", "Dev B"]]
     )
     client.general.list_playback_devices = MagicMock(
-        side_effect=[[["hw:Cccc,0,0", "Dev C"], ["hw:Dddd,0,0", "Dev D"]]]
+        return_value=[["hw:Cccc,0,0", "Dev C"], ["hw:Dddd,0,0", "Dev D"]]
     )
-    client.general.supported_device_types = MagicMock(side_effect=[["Alsa", "Wasapi"]])
+    client.general.supported_device_types = MagicMock(return_value=["Alsa", "Wasapi"])
     client.status = MagicMock()
-    client.status.rate_adjust = MagicMock(side_effect=[1.01])
-    client.status.buffer_level = MagicMock(side_effect=[1234])
-    client.status.clipped_samples = MagicMock(side_effect=[12])
-    client.status.processing_load = MagicMock(side_effect=[0.5])
+    client.status.rate_adjust = MagicMock(return_value=1.01)
+    client.status.buffer_level = MagicMock(return_value=1234)
+    client.status.clipped_samples = MagicMock(return_value=12)
+    client.status.processing_load = MagicMock(return_value=0.5)
     client.config = MagicMock()
-    client.config.active = MagicMock(side_effect=[SAMPLE_CONFIG])
+    client.config.active = MagicMock(return_value=SAMPLE_CONFIG)
+    client.config.file_path = MagicMock(return_value = os.path.join(TESTFILE_DIR, "config.yml"))
     client.versions = MagicMock()
-    client.versions.library = MagicMock(side_effect=["1.2.3"])
-    with patch("camilladsp.CamillaClient", client_constructor):
+    client.versions.library = MagicMock(return_value="1.2.3")
+    yield client_constructor
+
+@pytest.fixture
+def mock_app(mock_camillaclient):
+    with patch("camilladsp.CamillaClient", mock_camillaclient):
+        app = main.build_app(server_config)
+        yield app
+
+@pytest.fixture
+def mock_offline_app(mock_camillaclient):
+    mock_camillaclient._client.config.file_path =  MagicMock(return_value = None)
+    mock_camillaclient._client.general.state = MagicMock(side_effect = camilladsp.CamillaError)
+    with patch("camilladsp.CamillaClient", mock_camillaclient):
         app = main.build_app(server_config)
         yield app
 
@@ -92,6 +107,9 @@ def mock_app():
 def server(event_loop, aiohttp_client, mock_app):
     return event_loop.run_until_complete(aiohttp_client(mock_app))
 
+@pytest.fixture
+def offline_server(event_loop, aiohttp_client, mock_offline_app):
+    return event_loop.run_until_complete(aiohttp_client(mock_offline_app))
 
 @pytest.mark.asyncio
 async def test_read_volume(mock_request):
@@ -193,3 +211,22 @@ async def test_upload_and_delete(server, upload, delete, getfile):
     # try to download the deleted file
     resp = await server.get(getfile + filename)
     assert resp.status == 404
+
+
+@pytest.mark.asyncio
+async def test_active_config_online(server):
+    resp = await server.get("/api/getactiveconfigfile")
+    assert resp.status == 200
+    content = await resp.json()
+    print(content)
+    assert content["configFileName"] == "config.yml"
+    assert content["config"]["devices"]["samplerate"] == 44100
+
+@pytest.mark.asyncio
+async def test_active_config_offline(offline_server):
+    resp = await offline_server.get("/api/getactiveconfigfile")
+    assert resp.status == 200
+    content = await resp.json()
+    print(content)
+    assert content["configFileName"] == "config2.yml"
+    assert content["config"]["devices"]["samplerate"] == 48000
